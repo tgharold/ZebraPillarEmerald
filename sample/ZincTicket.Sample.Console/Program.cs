5808e80abf708e4790f2c0b4a268ae35491287b8
Thomas Harold <tgharold@gmail.com>
2020-02-18 11:14:03 -0500

    cleanup, add sample proj

---
+++ sample/ZincTicket.Sample.Console/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ZincTicket.Sample.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
