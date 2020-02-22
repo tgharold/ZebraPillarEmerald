18026daa1ae3fce176541b597167bc0481dd6f88
Thomas Harold <tgharold@gmail.com>
2020-02-21 21:05:13 -0500

    dump the list of users

--- src/ZebraPillarEmerald.Api/Controllers/UsersController.cs
+++ src/ZebraPillarEmerald.Api/Controllers/UsersController.cs
@@ -1,16 +1,28 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ZebraPillarEmerald.Client.Representations.Users;
+using ZebraPillarEmerald.Core.Database;
 
 namespace ZebraPillarEmerald.Api.Controllers
 {
     [Microsoft.AspNetCore.Mvc.Route("/users")]
     public class UsersController : Controller
     {
+        private readonly ZpeDbContext _dbContext;
+
+        public UsersController(
+            ZpeDbContext dbContext
+            )
+        {
+            _dbContext = dbContext;
+        }
+        
         public async Task<IActionResult> Index([FromQuery] UsersIndexRequest request)
         {
-            throw new NotImplementedException();            
+            var users = await _dbContext.Users.ToListAsync();
+            return Ok(users);
         }
 
         [Route("{id}")]
